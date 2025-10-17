573bd7532a33821aa0e5a999e24777359c800d2f
LY <2269320653@qq.com>
2025-10-17 10:49:40 +0800

    优化仪表盘链接显示

--- demotools.py
+++ demotools.py
@@ -743,7 +743,7 @@ def generate_dashboard_service():
     打开一个网页数据仪表盘，调用后你会得到一个打开成功消息
     """
     generate_dashboard()
-    return "仪表盘开启成功，请访问https://chatbot.sparkai.xin/查看"
+    return "仪表盘开启成功，请访问 https://chatbot.sparkai.xin/ 查看"
 
 
 if __name__ == "__main__":
